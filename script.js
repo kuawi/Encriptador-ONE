76ddaf59d8a7dcbec045088d6d55d3d50b87ded8
Jose L. Lozano <jllozano.mx@gmail.com>
2024-08-20 22:34:11 -0600

    Agrega funcionalidad para desencriptar

--- script.js
+++ script.js
@@ -5,22 +5,32 @@ const deseBtn = document.querySelector('#btn2');
 const outputBox = document.querySelector('#output-p1');
 
 encBtn.addEventListener('click', encriptar);
+deseBtn.addEventListener('click', desencriptar);
 
 function encriptar(){
 	let msg = inputBox.value;
 	let vocales = [...msg.matchAll(/[aeiou]/g)].map((e) => e[0]);
 	let sinVocales = msg.split(/[aeiou]/);
-	outputBox.textContent = makeString(sinVocales, vocales);
+	outputBox.textContent = makeString(sinVocales, vocales, true);
 }
 
-function makeString(arr1, arr2){
+function desencriptar(){
+	let msg = inputBox.value;
+	let reemplazables = [...msg.matchAll(/ai|enter|imes|ober|ufat/g)].map((e) => e[0]);
+	let vocales = reemplazables.map((str) => str[0]);
+	let sinVocales = msg.split(/ai|enter|imes|ober|ufat/);
+	console.log(reemplazables, vocales, sinVocales);
+	outputBox.textContent = makeString(sinVocales, vocales, false);
+}
+
+function makeString(arr1, arr2, mod){
 	let res = [];
 	let lim = arr1.length + arr2.length;
 	for(let i=0; i < lim; i++){
 		if (i%2 == 0) {
 			res.push(arr1.pop());
 		} else {
-			res.push(clave[arr2.pop()]);
+			res.push(mod ? clave[arr2.pop()] : arr2.pop());
 		}
 	}
 	return res.reverse().join('');
